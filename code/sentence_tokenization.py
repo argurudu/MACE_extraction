255e1bd9ea9a89b2e7a96e8f4801db4e449f6aa8
argurudu <141592537+argurudu@users.noreply.github.com>
2023-09-06 19:55:57 -0700

    Update sentence_tokenization.py

--- code/sentence_tokenization.py
+++ code/sentence_tokenization.py
@@ -1,1 +1,20 @@
+import pandas as pd
+import re
+
+'''
+Read the chosen dataset below.
+'''
+df = pd.read_csv(#include csv file)
+
+
+'''
+Sentence Tokenization:
+Split all reports into sentence tokens that are separated by a period, new line, or semicolon. Store these tokens in a new column. 
+'''
+
+allSentences = []
+for text in df[r'text']:
+    allSentences.append(re.split(r'\.|\n|;', text))
+df[r'sentence_tokens'] = allSentences
+
 
